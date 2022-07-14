22246bf334f38fd5398fb23d2cb5563945460acb
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-07-14 12:14:48 +0300

    new: implement languages from database

--- routers/language_router.js
+++ routers/language_router.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const hajDB = require("../models/haj_db");
 
 // define the home page route
-router.get('/', function(req, res) {
-    res.render("index.njk");
+router.get("/", function (req, res) {
+  let data = {};
+  res.render("index.njk", data);
 });
 
+// router.get("/", function (req, res) {
+//   let data = {
+//     languages: hajDB.getAllLanguages(),
+//   };
+//   res.render("base.njk", data);
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const language_router = require('./routers/language_router');
-const nunjucks = require('nunjucks');
-const cookieParser = require("cookie-parser")
-const bodyParser = require('body-parser');
+const language_router = require("./routers/language_router");
+const nunjucks = require("nunjucks");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-
-
-nunjucks.configure('./views', {
-    autoescape: true,
-    express: app
+nunjucks.configure("./views", {
+  autoescape: true,
+  express: app,
 });
 
 // app.use(cookieParser())
 // app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', language_router);
+app.use("/", language_router);
 
 app.use(express.static("public"));
 
-
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on http://localhost:%d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on http://localhost:%d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
